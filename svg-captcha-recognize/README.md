7d2b4f3aad452855ba8d02900320ef1ac618af6f
syy <2232825487@qq.com>
2020-08-03 14:15:04 +0800

    first commit

---
+++ svg-captcha-recognize/README.md
@@ -0,0 +1,46 @@
+# svg-captcha-recognize svg-captcha验证码识别
+
+## 项目说明
+
+该项目不能用于任何盗刷行为，仅为证实svg-captcha生成的验证码是可以很轻易被破解的，
+比位图的验证码更容易被破解。
+
+如果实在需要使用svg-captcha，请一定准备多套字体，并且经常更换，英文字体还是很容易找到的。
+
+ps. 我已经给svg-captcha提了issue了，看他们的重视程度，应该会修复这个问题。
+
+> issue 已经有回复了： https://github.com/produck/svg-captcha/issues/45
+3.0会完全修复
+
+**特点**：
+1. 识别正确率100%
+2. 每次识别耗时最高都在1ms以内
+3. 零依赖
+
+缺点：
+1. 仅支持 svg-captcha 默认的字体，如果用了自定义字体，需要重新生成字典
+2. 目前是针对 svg-captcha 1.4.0 及以下版本做的，不排除他们更新后此方法无法识别
+
+> Even though you can write a program that convert svg to png, svg captcha has done its job
+  —— make captcha recognition harder 
+
+> 所以SVG验证码可能比的图片普通验证码要更难识别，因为你必须先做SVG到其它格式的转化。
+
+以上引用了svg-captcha文档的一句话，而我并没有把SVG转为其它格式，却做到了更快的验证码识别，并且识别率100%。
+
+svg-captcha: https://www.npmjs.com/package/svg-captcha
+
+## 用法
+
+    const { recognize } = require('./index')
+    const text = recognize(csvText) // 只需传入由svg-captcha生成的svg字符串
+    console.log(text)
+
+## 测试方法
+
+    # 以下命令将不停地生成svg-captcha验证码，然后识别，
+    # 最后对比识别出来的验证码与svg-captcha返回的是否一致，
+    # 同时打印结果。当需要停止测试时，按下ctrl+c即可
+    npm install
+    node ./test.js
+    # 已分析83200次，识别成功数：83200 / 83200 (100.00%)，总耗时53 second 530 ms，平均每次耗时0.64 ms
---
+++ test.js
@@ -0,0 +1,96 @@
+let config = {
+  usr: "学号",//账号
+  pwd: "密码",//密码
+  vrCode: null,
+  body_tem: 36.9 + Math.round(Math.random() * 3 - 1) / 10,
+}
+
+const puppeteer = require('puppeteer');
+const { recognize } = require('./svg-captcha-recognize/index')
+const cookies = [
+  {
+    'domain': 'yq.nauvpn.cn',
+    //'path': '/',
+    'name': 'NSYQJK-ISSHOWTIPNEXTTIME',
+    'value': '{%22isNotShowNext%22:true%2C%22isAccept%22:true}',
+  },
+];
+const autoFillIn = async () => {
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.setCookie(...cookies)
+  await page.goto('http://yq.nauvpn.cn/login', {
+    waitUntil: 'networkidle0'
+  });
+  //验证码
+  const vrCodeVR = async () => {
+    let html = await page.content();
+    let svg_dom = ""; html.replace(/<svg.+<\/svg>/, (all) => svg_dom = all)
+    config.vrCode = recognize(svg_dom)
+    console.log("confirm vrCode: " + config.vrCode)
+    if (config.vrCode.length <= 3) {
+      //需要重新获取验证码
+      await page.click("svg")
+      await page.waitFor(1000)
+      await vrCodeVR()
+    }
+  }
+  await vrCodeVR()
+
+  await page.type("input[placeholder='请输入学号']", config.usr)
+  await page.type("input[placeholder='请输入密码']", config.pwd)
+  await page.type("input[placeholder='请输入验证码']", config.vrCode)
+
+
+
+  await page.click("button[type='button']")
+
+  const nextPageVR = async () => {
+    try {
+      await page.waitForSelector("input[placeholder='请选择地址']", {
+        waitUntil: 'networkidle0',
+        timeout: 5000,
+      })
+      return 0
+    } catch (ex) {
+      const btnName = await page.$eval('button > span', el => el.innerHTML)
+      if (btnName == "关闭") {
+        return 1
+      }
+      return -1
+    }
+  }
+  let login_state = await nextPageVR()
+  if (login_state == -1) {
+    console.log("restart server...")
+    await page.waitFor(500)
+    await browser.close();
+    return login_state
+  }
+  if(login_state==1){
+    console.log("already fill in !")
+    await page.waitFor(1000)
+    await browser.close();
+    return login_state
+  }
+
+  console.log("success login !")
+  await page.waitFor(2000)
+  await page.type("input[placeholder='请输入今日体温']", config.body_tem + "", { delay: 3 })
+  await page.click("button[type='button']")
+  console.log('success!')
+  await page.waitFor(2000)
+  await browser.close();
+  return 0;
+}
+
+
+async function run() {
+  let cc = await autoFillIn()
+  if (cc == -1) return await run()
+  return 0
+
+}
+(async () => {
+  await run()
+})()
